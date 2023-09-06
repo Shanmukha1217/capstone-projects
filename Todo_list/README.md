1cc885bc8d29f942fba944874533d58a011e0d0d
Shanmukha1217 <shanmukhads7@gmail.com>
2023-09-06 20:24:24 +0530

    DB attached for the taks

--- Todo_list/README.md
+++ Todo_list/README.md
@@ -1,1 +1,9 @@
-##Creating todo app with EJS templating, Node and Express
+## Creating todo app with EJS templating, Node and Express and Mongoose(MongoDB)
+
+# In this Todo list We have below features
+
+# 1. Add and Delete Tasks. (at first we'll get 3 default informatory tasks).
+
+# 2. We can create some custom list of tasks based on need (ex: if we need separate list of tasks for ex: Home, Work, Farm, so on.. )
+
+# to achieve the same we need to change the root accordingly in the adderss bar (ex : /home, /work, /farm)
--- Todo_list/app.js
+++ Todo_list/app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -11,7 +10,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose
+  .connect("mongodb://localhost:27017/todolistDB")
+  .then(() => console.log("DB connection successfull"))
+  .catch(() => console.log("Unable to connedt to DB"));
 
 const itemsSchema = {
   name: String,
@@ -20,46 +22,108 @@ const itemsSchema = {
 const Item = mongoose.model("Item", itemsSchema);
 
 const item1 = new Item({
-  name: "Get up",
+  name: "Welcome to new Todo List",
 });
 const item2 = new Item({
-  name: "Fresh up",
+  name: "Press + button to add new Task",
 });
 const item3 = new Item({
-  name: "Start Coding",
+  name: "<-- Click this  to remove the  task",
 });
 
 const defaultItems = [item1, item2, item3];
 
-console.log(Item.find({}));
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+const List = mongoose.model("list", listSchema);
+app.get("/", async (req, res) => {
+  try {
+    // Retrieve data from MongoDB using Mongoose
+    const items = await Item.find({});
 
-app.get("/", function (req, res) {
-  res.render("list", { listTitle: "Today", newListItems: items });
-});
-
-app.post("/", function (req, res) {
-  const item = req.body.newItem;
-  if (item) {
-    items.push(item);
+    if (items.length === 0) {
+      Item.insertMany(defaultItems);
+      res.redirect("/");
+    } else {
+      // Render the "list" template with the retrieved data
+      res.render("list", { listTitle: "Today", newListItems: items });
+    }
+  } catch (err) {
+    console.error("Error retrieving data:", err);
+    res.status(500).send("Error retrieving data");
   }
-
-  res.redirect("/");
 });
 
-app.get("/work", function (req, res) {
-  res.render("work", { listTitle: "Work List", newListItems: workItems });
-});
-app.post("/work", function (req, res) {
-  const item = req.body.newItem;
-  if (item) {
-    workItems.push(item);
+app.post("/", async function (req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const newItem = new Item({
+    name: itemName,
+  });
+
+  if (listName === "Today") {
+    newItem.save().then(() => console.log("Item Saved Successfully"));
+    res.redirect("/");
+  } else {
+    await List.findOne({ name: listName }).then((data) => {
+      data.items.push(newItem);
+      data.save();
+      console.log("Item Saved Successfully");
+    });
+    res.redirect(`/${listName}`);
   }
-
-  res.redirect("/work");
 });
 
-app.get("/about", function (req, res) {
-  res.render("about");
+//Custom list for custom routes by limiting only one list for one route
+
+app.get("/:customListName", async (req, res) => {
+  const customListName = _.capitalize(req.params.customListName);
+
+  await List.findOne({ name: customListName })
+    .then((data) => {
+      if (!data) {
+        // if no list founds in the DB creating one with some default items
+
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+
+        list.save();
+        res.redirect(`/${customListName}`);
+      } else {
+        res.render("list", { listTitle: data.name, newListItems: data.items });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+app.post("/delete", async (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+  if (listName === "Today") {
+    await Item.findByIdAndRemove(checkedItemId)
+      .then(() => {
+        console.log("Record Removed Successfully");
+      })
+      .catch((err) => console.log("Error:", err));
+
+    res.redirect("/");
+  } else {
+    await List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: checkedItemId } } }
+    )
+      .then(() => {
+        console.log("Record Removed Successfully");
+      })
+      .catch((err) => console.log("Error:", err));
+
+    res.redirect(`/${listName}`);
+  }
 });
 
 app.listen(3000, function () {
