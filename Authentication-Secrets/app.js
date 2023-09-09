66dc586e8a79322e4324ed15537ff66a3eda91be
Shanmukha1217 <shanmukhads7@gmail.com>
2023-09-09 19:40:48 +0530

    Hashing + Salting with bcrypt

--- Authentication-Secrets/app.js
+++ Authentication-Secrets/app.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.set("view engine", "ejs");
@@ -42,28 +42,35 @@ app.get("/login", async (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
 
-  newUser
-    .save()
-    .then(() => {
-      console.log("User added to DB");
-      res.render("secrets");
-    })
-    .catch((err) => res.send(err));
+    newUser
+      .save()
+      .then(() => {
+        console.log("User added to DB");
+        res.render("secrets");
+      })
+      .catch((err) => res.send(err));
+  });
 });
 
 app.post("/login", async (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   await User.findOne({ email: username })
     .then((foundUser) => {
-      if (foundUser.password == password) {
-        res.render("secrets");
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       } else {
         res.send("No Credentials found");
       }
